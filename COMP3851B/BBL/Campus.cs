f3969e1bccd8562247c4fb1ee92ec758add5e823
gtan8510 <106289192+gtan8510@users.noreply.github.com>
2022-10-19 22:03:40 +0800

    Add files via upload

---
+++ COMP3851B/BBL/Campus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP3851B.BBL
+{
+    public class Campus
+    {
+        public int campusID { get; set; }
+        public  string campusLoc{ get; set; }
+        public string campusImage { get; set; }
+  
+        public Campus()
+        {
+
+        }
+
+        public Campus(int campusid, string campusloc, string campusimage)
+        {
+            this.campusID = campusid;
+            this.campusLoc = campusloc;
+            this.campusImage = campusimage;
+        }
+
+        public Campus(string campusloc, string campusimage)
+        {
+            this.campusLoc = campusloc;
+            this.campusImage = campusimage;
+        }
+            
+            
+            }
+    
+}
---
+++ COMP3851B/BBL/Feedback.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP3851B.BBL
+{
+    public class Feedback
+    {
+        public int feedBackID { get; set; }
+        public int stdID { get; set; }
+        public string stdName { get; set; }
+        public string stdFeedback { get; set; }
+         public Feedback()
+         {
+
+         }  
+
+        public Feedback(int feedbackid, int stdid, string stdname, string stdfeedback)
+        {
+            this.feedBackID = feedbackid;
+            this.stdID = stdid;
+            this.stdName = stdname;
+            this.stdFeedback = stdFeedback;
+        }
+
+        public Feedback(int stdid, string stdname, string stdfeedback)
+        {
+            this.stdID = stdid;
+            this.stdName = stdname;
+            this.stdFeedback = stdfeedback;
+        }
+
+        public Feedback(int stdid, int feedbackid, string stdfeedback)
+        {
+            this.stdID = stdid;
+            this.feedBackID = feedbackid;
+            this.stdFeedback = stdfeedback;
+        }
+    
+    
+    }
+
+   
+}
